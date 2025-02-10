9899dfc5f2f10dc8acfce2493bef73bc092d07a9
김주엽 <kjyy08@naver.com>
2025-02-10 14:28:40 +0900

    ✨ Feat: form 추가

--- docs/app.js
+++ docs/app.js
@@ -1,13 +1,28 @@
 async function main() {
-  async function handleScheduleBtn() {
-    document.querySelector("#box").textContent = await (
-      await fetch("http://127.0.0.1:3000")
-    ).text();
+  async function handleScheduleBtn(event) {
+    event.preventDefault();
+    const url = "http://127.0.0.1:3000";
+    const formData = new FormData(document.querySelector("#schedule-form"));
+    const text = formData.get("text");
+
+    console.log("버튼눌림");
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        text,
+      }),
+    });
+
+    const jsonData = await response.json();
+    document.querySelector("#box").textContent = JSON.stringify(jsonData);
   }
 
   document
-    .querySelector("#schedule-btn")
-    .addEventListener("click", handleScheduleBtn);
+    .querySelector("#schedule-form")
+    .addEventListener("submit", handleScheduleBtn);
 }
 
 document.addEventListener("DOMContentLoaded", main);
